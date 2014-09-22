128ba5e4c9aa05c9f1125802334d2fd0b01a22a5
Ubuntu <ubuntu@ip-172-31-41-7.us-west-2.compute.internal>
2014-09-22 15:21:47 +0000

    modified my-channel with console.log as a workaround

--- collections/posts.js
+++ collections/posts.js
@@ -12,9 +12,9 @@ Schemas.Posts = new SimpleSchema({
     excerpt: {
         type: String,
         label: "Excerpt",
-        max: 3000,
+        max: 2000,
 		autoform: {
-		  rows: 10
+		  rows: 5
 		}
     },
     url: {
@@ -33,27 +33,24 @@ Schemas.Posts = new SimpleSchema({
     },
     feedId: {
         type: String,
-        label: "Feed ID",
-		denyUpdate: true
+        label: "Feed ID"
     },
     originalId: {
     	type: String,
-    	label: "ID at feed source",
-		denyUpdate: true
+    	label: "ID at feed source"
     },
     userId: {
     	type: String,
+		label: "User ID",
     	optional: false
     },
     votes: {
         type: String,
-        label: "Votes",
-		denyUpdate: true
+        label: "Votes"
     },
     commentsCount: {
         type: String,
-        label: "Comments",
-		denyUpdate: true
+        label: "Comments"
     },
     originalPostDate: {
         type: Date,
@@ -72,65 +69,3 @@ Schemas.Posts = new SimpleSchema({
 });
 
 Posts.attachSchema(Schemas.Posts);
-
-Posts.allow({
-  update: ownsDocument,
-  remove: ownsDocument
-});
-
-Posts.deny({
-  update: function(userId, post, fieldNames) {
-    // may only edit the following two fields:
-    return (_.without(fieldNames, 'url', 'title', 'channels').length > 0);
-  }
-});
-
-Meteor.methods({
-  post: function(postAttributes) {
-    var user = Meteor.user(),
-      postWithSameLink = Posts.findOne({url: postAttributes.url});
-    
-    // ensure the user is logged in
-    if (!user)
-      throw new Meteor.Error(401, "You need to login to post new stories");
-    
-    // ensure the post has a title
-    if (!postAttributes.title)
-      throw new Meteor.Error(422, 'Please fill in a headline');
-    
-    // check that there are no previous posts with the same link
-    if (postAttributes.url && postWithSameLink) {
-      throw new Meteor.Error(302, 
-        'This link has already been posted', 
-        postWithSameLink._id);
-    }
-    
-    // pick out the whitelisted keys
-    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message','channels'), {
-      userId: user._id, 
-      author: user.username, 
-      submitted: new Date().getTime(),
-      commentsCount: 0,
-      upvoters: [], votes: 0
-    });
-    
-    var postId = Posts.insert(post);
-    
-    return postId;
-  },
-  
-  upvote: function(postId) {
-    var user = Meteor.user();
-    // ensure the user is logged in
-    if (!user)
-      throw new Meteor.Error(401, "You need to login to upvote");
-    
-    Posts.update({
-      _id: postId, 
-      upvoters: {$ne: user._id}
-    }, {
-      $addToSet: {upvoters: user._id},
-      $inc: {votes: 1}
-    });
-  }
-});
